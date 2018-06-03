14083911c5bf561cb5f05b15d27cf5735f0d9018
ph9214 <ph9214@gmail.com>
2018-06-03 00:06:17 -0600

    Fonts First!!!

---
+++ new-arch-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Install My Stuff
+
+#chinese fonts - for ascii art ;)
+yay -S adobe-source-han-sans-cn-fonts; yay -S adobe-source-han-sans-tw-fonts; yay -S adobe-source-han-serif-cn-fonts; yay -S adobe-source-han-serif-tw-fonts; yay -S noto-fonts-sc; yay -S noto-fonts-tc; yay -S wqy-microhei; yay -S wqy-zenhei; yay -S wqy-bitmapfont; yay -S ttf-arphic-ukai; yay -S ttf-arphic-uming; yay -S opendesktop-fonts; yay -S ttf-hannom; yay -S ttf-tw; yay -S ttf-twcns-fonts; yay -S ttf-ms-win8-zh_cn ; yay -S ttf-ms-win8-zh_tw ; yay -S ttf-ms-win10-zh_cn; yay -S ttf-ms-win10-zh_tw; yay -S ttf-i.bming
