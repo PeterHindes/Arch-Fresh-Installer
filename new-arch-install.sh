db432d3a0e71686783ba05d6db1dbc6c8984a0e2
ph9214 <ph9214@gmail.com>
2018-06-03 12:14:31 -0600

    plans

--- new-arch-install.sh
+++ new-arch-install.sh
@@ -2,5 +2,19 @@
 
 # Install My Stuff
 
-#chinese fonts - for ascii art ;)
+# chinese fonts - for ascii art ;)
 yay -S adobe-source-han-sans-cn-fonts; yay -S adobe-source-han-sans-tw-fonts; yay -S adobe-source-han-serif-cn-fonts; yay -S adobe-source-han-serif-tw-fonts; yay -S noto-fonts-sc; yay -S noto-fonts-tc; yay -S wqy-microhei; yay -S wqy-zenhei; yay -S wqy-bitmapfont; yay -S ttf-arphic-ukai; yay -S ttf-arphic-uming; yay -S opendesktop-fonts; yay -S ttf-hannom; yay -S ttf-tw; yay -S ttf-twcns-fonts; yay -S ttf-ms-win8-zh_cn ; yay -S ttf-ms-win8-zh_tw ; yay -S ttf-ms-win10-zh_cn; yay -S ttf-ms-win10-zh_tw; yay -S ttf-i.bming
+
+# Use i3-dmenu-desktop on alt d
+
+# Use a number=display and letter=workspace method instead of number=workspace method for i3
+
+# Media Controlls i3
+
+# Power Controlls i3
+
+# Wokspace Renaming?
+
+# Static netowrk config through dhcpcd
+
+# DNS Settings through resolv conf
