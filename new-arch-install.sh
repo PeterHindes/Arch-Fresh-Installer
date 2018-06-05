6279af6c468511b3b843bde7cdbf654420d33290
Peter Hindes <ph9214@gmail.com>
2018-06-05 13:02:46 -0600

    Some Stuff

--- new-arch-install.sh
+++ new-arch-install.sh
@@ -2,8 +2,25 @@
 
 # Install My Stuff
 
+# Wifi and ethernet
+yay -S pacman -S dialog wpa_actiond ifplugd wpa_suppicant
+#ask user for wifi and ethernet cards
+systemctl enable netctl-auto@$wificard
+systemctl enable netctl-ifplugd@$ethernetcard
+
 # chinese fonts - for ascii art ;)
 yay -S adobe-source-han-sans-cn-fonts; yay -S adobe-source-han-sans-tw-fonts; yay -S adobe-source-han-serif-cn-fonts; yay -S adobe-source-han-serif-tw-fonts; yay -S noto-fonts-sc; yay -S noto-fonts-tc; yay -S wqy-microhei; yay -S wqy-zenhei; yay -S wqy-bitmapfont; yay -S ttf-arphic-ukai; yay -S ttf-arphic-uming; yay -S opendesktop-fonts; yay -S ttf-hannom; yay -S ttf-tw; yay -S ttf-twcns-fonts; yay -S ttf-ms-win8-zh_cn ; yay -S ttf-ms-win8-zh_tw ; yay -S ttf-ms-win10-zh_cn; yay -S ttf-ms-win10-zh_tw; yay -S ttf-i.bming
+
+# Sound
+yay -S alsa-firmware alsa-utils alsa-plugins pulseaudio-alsa pulseaudio pavucontrol
+
+# i3
+pacman -S i3 dmenu xorg xorg-xinit
+echo \#\!"/bin/bash" >> vim ~/.xinitrc
+echo exec i3 >> vim ~/.xinitrc
+
+# Termial
+yay -S terminator
 
 # Use i3-dmenu-desktop on alt d
 
