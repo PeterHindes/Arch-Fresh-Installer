d98ac14b22e855ad74d30b05ef0027e1348b545c
Peter Hindes <ph9214@gmail.com>
2018-06-05 18:33:32 -0600

    sed angle

--- inside.sh
+++ inside.sh
@@ -17,19 +17,22 @@ hwclock --systohc --utc
 
 echo $hname > /etc/hostname
 
-passwd
+echo def | passwd --stdin
 
 useradd -m -g users -G wheel,games,power,optical,storage,scanner,lp,audio,video -s /bin/bash peter
 
-passwd peter
+echo def | passwd peter --stdin
 
 pacman -S sudo grub efibootmgr os-prober
 
-EDITOR=nano visudo
+#EDITOR=nano visudo
+sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cryptdevice=/dev/sda2:cryptroot"/g' /etc/sudoers
 
-nano /etc/default/grub
+#nano /etc/default/grub
+sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cryptdevice=/dev/sda2:cryptroot"/g' /etc/default/grub
 
-nano /etc/mkinitcpio.conf
+#nano /etc/mkinitcpio.conf
+sed -i 's/block filesystems/block encrypt filesystems/g' /etc/mkinitcpio.conf
 
 mkinitcpio -p linux
 
