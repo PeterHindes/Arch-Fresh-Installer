7b2e0542195f7a0ba504b8255fbe36678121c673
Peter Hindes <ph9214@gmail.com>
2018-06-08 17:14:02 -0600

    run as user

--- laptop-minimum.sh
+++ laptop-minimum.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # yay
-pacman -S git
+sudo pacman -S git
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg
 pacman -U *.pkg.tar.xz
 
 # Wifi and ethernet
-yay -S pacman -S dialog wpa_actiond ifplugd wpa_suppicant
+sudo pacman -S dialog wpa_actiond ifplugd wpa_supplicant
 #ask user for wifi and ethernet cards
-systemctl enable netctl-auto@$wificard
-systemctl enable netctl-ifplugd@$ethernetcard
+sudo systemctl enable netctl-auto@$wificard
+sudo systemctl enable netctl-ifplugd@$ethernetcard
