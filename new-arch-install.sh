425eba919cb58cd540d6e2fd62176e7858c2b991
Peter Hindes <ph9214@gmail.com>
2018-06-08 17:02:14 -0600

    k

--- new-arch-install.sh
+++ new-arch-install.sh
@@ -2,6 +2,13 @@
 
 # Install My Stuff
 
+# yay
+pacman -S git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg
+pacman -U *.pkg.tar.xz
+
 # Wifi and ethernet
 yay -S pacman -S dialog wpa_actiond ifplugd wpa_suppicant
 #ask user for wifi and ethernet cards
